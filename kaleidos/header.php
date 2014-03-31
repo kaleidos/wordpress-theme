5c591a5db0147a243adef4280ff0f3ec8a0f782a
Xavier Julián <xavier.julian@kaleidos.net>
2014-03-31 12:57:06 +0200

    Language selector

--- kaleidos/header.php
+++ kaleidos/header.php
@@ -26,6 +26,30 @@
                 ?>
             </div>
 
+            <div class="language-selector">
+                <?php
+                    $terms = get_terms( 'lang', array(
+                        'slug' => 'es'
+                    ) );
+                    foreach ( $terms as $term ) {
+                        $term = sanitize_term( $term, 'lang' );
+                        $term_link = get_term_link( $term, 'lang' );
+                        echo '<a href="' . esc_url( $term_link ) . '">' . $term->name . '</a>';
+                    }
+                ?>
+                <span class="icon icon-world"></span>
+                <?php
+                    $terms = get_terms( 'lang', array(
+                        'slug' => 'en'
+                    ) );
+                    foreach ( $terms as $term ) {
+                        $term = sanitize_term( $term, 'lang' );
+                        $term_link = get_term_link( $term, 'lang' );
+                        echo '<a href="' . esc_url( $term_link ) . '">' . $term->name . '</a>';
+                    }
+                ?>
+            </div>
+
             <div class="site-branding">
                 <h1 class="site-title">
                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" title="<?php bloginfo( 'name' ); ?>">
