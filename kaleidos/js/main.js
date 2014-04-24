d238bc31930e05ad0ace99bc614f992cbda128f6
Xavier Julián <xavier.julian@kaleidos.net>
2014-04-24 15:17:27 +0200

    Remove resposive header parallax on heade mobile

--- kaleidos/js/main.js
+++ kaleidos/js/main.js
@@ -9,22 +9,24 @@
     $(window).scroll(function() {
         /* Parallax */
         var $window = $(window);
-        /* */
         var documentHeight = $(document).height();
         var windowHeight = $window.height();
         var scrollTop = $window.scrollTop();
 
-        $window.on("scroll", function() {
-            scrollTop = $(window).scrollTop();
-            var scrollToOne = (scrollTop) / (documentHeight - windowHeight); //gives us the scroll percent range from 0 to 1
-            var scrollPercent = Math.round(scrollToOne*100) +30;
-            var $bgobj = $('.site-branding-bg');
-            var coords = 'center '+ (scrollPercent) + '%';
+        if ($(window).width() > 1000){
+            $window.on("scroll", function() {
+                scrollTop = $(window).scrollTop();
+                var scrollToOne = (scrollTop) / (documentHeight - windowHeight); //gives us the scroll percent range from 0 to 1
+                var scrollPercent = Math.round(scrollToOne*100) +30;
+                var $bgobj = $('.site-branding-bg');
+                var coords = 'center '+ (scrollPercent) + '%';
 
-            console.log(coords);
-            $bgobj.css({ backgroundPosition: coords });
-        });
-        /**/
+                console.log(coords);
+                $bgobj.css({ backgroundPosition: coords });
+            });
+        }
+
+
         /* Search fixed */
         var headerHeight = $('.site-header').height() + 41;
         if (scrollTop > headerHeight) {
